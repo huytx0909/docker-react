449fd61f055838231ca28e9c696c246b3b975973
huytx0909 <huytx@smartosc.com>
2019-12-31 15:12:01 +0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('something cool here', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
